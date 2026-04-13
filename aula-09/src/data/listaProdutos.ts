b2029118272a57f6c161ebb5447575775fd85e90
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-04-13 11:23:32 -0300

    feat : Exemplo de carregamento com useEffect e useState
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

---
+++ aula-09/src/data/listaProdutos.ts
@@ -0,0 +1,6 @@
+
+export const listaProdutos = [
+    {id:1, nome:"Monitor", preco: 500},
+    {id:2, nome:"Mouse", preco: 100},
+    {id:3, nome:"Teclado", preco: 200},
+]
--- aula-09/src/routes/Produtos/index.tsx
+++ aula-09/src/routes/Produtos/index.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react"
+import { listaProdutos } from "../../data/listaProdutos";
+
 export default function Produtos() {
+
+useEffect(() => {
+    console.log("CARREGADO");
+  });
+
+  useEffect(() => {
+    listaProdutos.forEach(p=> console.log("PRODUTO : " + p.nome));
+  },[]);
+
+  const [contador, setContador] = useState(0)
+
+  useEffect(()=>(
+    console.log(`CONTADOR : ${contador}`)
+  ),[contador]);
+  
   return (
     <main>
-        <h1>Produtos</h1>
+      <h1>Produtos</h1>
+      <div>
+        <button onClick={()=> setContador(contador + 1)}>EXECUTE</button>
+      </div>
     </main>
   )
 }
- 
